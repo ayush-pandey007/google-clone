247f8f59249ab64583fc46db43233adb6cc430bf
ayush-pandey007 <ayushpandey1151@gmail.com>
2022-01-31 10:54:15 +0530

    fix some more bugs

--- key.js
+++ key.js
@@ -1,7 +1,1 @@
-export const API_KEY = "AIzaSyBpRqQgTykB5WeP5J1pUsrba1ZHEfLESEY";
 
-export const CONTEXT_KEY = "cd373704bc306b0fb";
-//AIzaSyBpRqQgTykB5WeP5J1pUsrba1ZHEfLESEY
-/* <script async src="https://cse.google.com/cse.js?cx=cd373704bc306b0fb"></script>
-<div class="gcse-search"></div> */
-//}
